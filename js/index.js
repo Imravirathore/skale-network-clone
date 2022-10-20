85183e0c4ea4c8c6e11a8b6dd9b60ea588f0a45c
Imravirathore <ravirathore672@gmail.com>
2022-10-20 20:50:44 +0530

    intital files

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+var btn = document.getElementById("demo")
+var sideMenu = document.getElementById("sideMenu")
+ 
+
+btn.addEventListener('click', function(){
+
+    sideMenu.classList.toggle("new")
+
+
+ })
+
